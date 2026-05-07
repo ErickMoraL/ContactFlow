5b326f62182e8329d8d44431227e7b0bc5467d62
ErickM <erickmora0503@gmail.com>
2026-05-07 05:06:04 +0000

    feat: add contacts app with contact list view and templates, update main URL routing

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from .models import Contact
+
+
+def contact_list(request):
+    contacts = Contact.objects.all()
+    return render(request, "contact_list.html", {"contacts": contacts})
