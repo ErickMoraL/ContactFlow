e483528de46a47d9dc925756f54f213d1985f5bc
ErickM <erickmora0503@gmail.com>
2026-05-14 05:05:32 +0000

    feat: implement contact management features with forms, templates, and views for creating, editing, and listing contacts

--- contacts/urls.py
+++ contacts/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.contact_list, name="contact_list"),
+    path("<int:pk>/", views.contact_detail, name="contact_detail"),
+    path("create", views.contact_create, name="contact_create"),
+    path("<int:pk>/edit", views.contact_edit, name="contact_edit"),
+    path("delete_selected", views.contact_delete, name="contact_delete_selected"),
+    path("mark_closed", views.contact_mark_closed, name="contact_mark_closed"),
+    path("mark_lost", views.contact_mark_lost, name="contact_mark_lost"),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,10 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
 from django.http import HttpResponse
 from .models import Contact
+from django.db.models import Q
+from .forms import ContactForm
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def contact_list(request):
-    contacts = Contact.objects.all()
+
+    query = request.GET.get("q", "")
+
+    contacts = Contact.objects.prefetch_related("emails", "phones").all()
+
+    if query:
+        contacts = contacts.filter(
+            Q(first_name__icontains=query)
+            | Q(last_name__icontains=query)
+            | Q(company__name__icontains=query)
+            | Q(status__icontains=query)
+        )
+
     return render(request, "contact_list.html", {"contacts": contacts})
+
+
+@login_required
+def contact_detail(request, pk):
+    contact = Contact.objects.get(pk=pk)
+
+    return render(request, "contact_detail.html", {"contact": contact})
+
+
+@login_required
+def contact_create(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("contact_list")
+    else:
+        form = ContactForm()
+
+    return render(request, "contact_create.html", {"form": form})
+
+@login_required
+def contact_edit(request, pk):
+    contact = Contact.objects.get(pk=pk)
+
+    return render(request, "contact_edit.html", {"contact": contact})
+
+@login_required
+def contact_delete(request):
+    return HttpResponse("Delete selected contacts")
+
+@login_required
+def contact_mark_closed(request):
+    return HttpResponse("Mark selected contacts as closed")
+
+
+@login_required
+def contact_mark_lost(request):
+    return HttpResponse("Mark selected contacts as lost")
